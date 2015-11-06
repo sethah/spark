889abd015f52ee545161cc3162bb723ee5aa042c
sethah <seth.hendrickson16@gmail.com>
2015-11-06 12:54:02 -0800

    minor changes

--- mllib/src/main/scala/org/apache/spark/ml/regression/DecisionTreeRegressor.scala
+++ mllib/src/main/scala/org/apache/spark/ml/regression/DecisionTreeRegressor.scala
@@ -30,8 +30,6 @@ import org.apache.spark.mllib.tree.model.{DecisionTreeModel => OldDecisionTreeMo
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 
-import org.apache.spark.mllib.tree.configuration.Strategy
-
 /**
  * :: Experimental ::
  * [[http://en.wikipedia.org/wiki/Decision_tree_learning Decision tree]] learning algorithm
@@ -85,10 +83,9 @@ final class DecisionTreeRegressor @Since("1.4.0") (@Since("1.4.0") override val 
     trees.head.asInstanceOf[DecisionTreeRegressionModel]
   }
 
-  // added so we can train from an rdd and not dataframe
-  // alternatively we could just convert the rdd to a dataframe
+  /** (private[ml]) Train a decision tree on an RDD */
   private[ml] def trainOld(data: RDD[LabeledPoint],
-      oldStrategy: Strategy): DecisionTreeRegressionModel = {
+      oldStrategy: OldStrategy): DecisionTreeRegressionModel = {
     val trees = RandomForest.run(data, oldStrategy, numTrees = 1, featureSubsetStrategy = "all",
       seed = 0L, parentUID = Some(uid))
     trees.head.asInstanceOf[DecisionTreeRegressionModel]
@@ -136,7 +133,6 @@ final class DecisionTreeRegressionModel private[ml] (
   private[ml] def this(rootNode: Node, numFeatures: Int) =
     this(Identifiable.randomUID("dtr"), rootNode, numFeatures)
 
-  // TODO: add this change to DT classifier too?
   override protected def predict(features: Vector): Double = {
     rootNode.predictImpl(features).prediction
   }
